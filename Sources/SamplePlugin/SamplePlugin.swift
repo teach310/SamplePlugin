baddd49f6a9c78c6980f61a2c63a30cd09b7804a
teach310 <okms310@yahoo.co.jp>
2023-06-17 21:41:26 +0900

    Csharp から 歩数を取得するための関数を定義
    https://qiita.com/fuziki/items/b51353e9fe8f59640cc3

--- Sources/SamplePlugin/SamplePlugin.swift
+++ Sources/SamplePlugin/SamplePlugin.swift
@@ -5,3 +5,21 @@ public func sample_plugin_helloworld() -> Int32 {
     print("Hello World!")
     return 3
 }
+
+#if os(iOS)
+public typealias SamplePluginAuthorizeCompletion = @convention(c) (Bool) -> Void
+
+@_cdecl("sample_plugin_authorize")
+public func sample_plugin_authorize(_ completion: @escaping SamplePluginAuthorizeCompletion) {
+    HealthKitData().authorize(completion: completion)
+}
+
+public typealias SamplePluginGetStepsTodayCompletion = @convention(c) (Int32) -> Void
+
+@_cdecl("sample_plugin_get_steps_today")
+public func sample_plugin_get_steps_today(_ completion: @escaping SamplePluginGetStepsTodayCompletion) {
+    HealthKitData().getStepsToday { steps in
+        completion(Int32(steps))
+    }
+}
+#endif
