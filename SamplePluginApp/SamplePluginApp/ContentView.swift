c6a42505e52894b303e8f7802fa71e7b225d18bf
teach310 <okms310@yahoo.co.jp>
2023-07-25 22:31:19 +0900

    scan
    ## NSObjectを継承している理由
    
    しないとCBCentralManagerDelegateを実装するとエラーでるから。
    
    https://developer.apple.com/documentation/corebluetooth
    
    ここに iOS13以降であればNSBluetoothAlwaysUsageDescriptionが必要と書かれている。
    
    ## cbManagerStateNameを定義している理由
    
    これ定義しないと rawValue 2のように表示されてなんだかわからない。

--- SamplePluginApp/SamplePluginApp/ContentView.swift
+++ SamplePluginApp/SamplePluginApp/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
 
 
             Button(action: {
-                bleSample.helloWorld()
+                bleSample.scan()
             }) {
                 Text("Tap me!")
                     .font(.title)
--- Sources/SamplePlugin/BLESample.swift
+++ Sources/SamplePlugin/BLESample.swift
@@ -1,10 +1,65 @@
 import Foundation
+import CoreBluetooth
 
-public class BLESample {
-    public init() {
+public class BLESample: NSObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    var serviceUUID: CBUUID!
+    
+    public override init() {
+        super.init()
+        // queueはnilの場合はメインスレッドで実行される
+        // https://developer.apple.com/documentation/corebluetooth/cbcentralmanager/1519001-init
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        serviceUUID = CBUUID(string: "068c47b7-fc04-4d47-975a-7952be1a576f")
     }
-
-    public func helloWorld() {
-        print("Hello World!")
+    
+    public func scan() {
+        if centralManager.state != .poweredOn {
+            print("Bluetooth is not powered on.")
+            return
+        }
+        
+        if centralManager.isScanning {
+            print("Already scanning.")
+            return
+        }
+        
+        let options: [String: Any] = [
+            CBCentralManagerScanOptionAllowDuplicatesKey: false
+        ]
+        
+        // serviceUUIDを指定すると、指定したサービスを持つペリフェラルのみをスキャンする。(推奨設定)
+        // https://developer.apple.com/documentation/corebluetooth/cbcentralmanager/1518986-scanforperipherals
+        centralManager.scanForPeripherals(withServices: [serviceUUID], options: options)
+    }
+    
+    func cbManagerStateName(_ state: CBManagerState) -> String {
+        switch state {
+        case .unknown:
+            return "unknown"
+        case .resetting:
+            return "resetting"
+        case .unsupported:
+            return "unsupported"
+        case .unauthorized:
+            return "unauthorized"
+        case .poweredOff:
+            return "poweredOff"
+        case .poweredOn:
+            return "poweredOn"
+        @unknown default:
+            return "unknown default"
+        }
+    }
+    
+    // MARK: - CBCentralManagerDelegate
+    
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("centralManagerDidUpdateState: \(cbManagerStateName(central.state))")
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        print("Device found: \(peripheral)")
+        central.stopScan()
     }
 }
