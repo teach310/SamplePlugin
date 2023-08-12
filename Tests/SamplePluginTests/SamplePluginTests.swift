1d891afe8c4f36fa434119195b711495e0c55b9f
teach310 <okms310@yahoo.co.jp>
2023-08-13 00:22:43 +0900

    remove test

--- Tests/SamplePluginTests/SamplePluginTests.swift
+++ Tests/SamplePluginTests/SamplePluginTests.swift
@@ -6,6 +6,6 @@ final class SamplePluginTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SamplePlugin().text, "Hello, World!")
+//        XCTAssertEqual(SamplePlugin().text, "Hello, World!")
     }
 }
