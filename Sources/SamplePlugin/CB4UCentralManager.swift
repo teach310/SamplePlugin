2a427e5c34d235d73017cc13d15363bff08d5649
teach310 <okms310@yahoo.co.jp>
2023-08-14 23:05:59 +0900

    スキャンを実装
    instancePtrだと複数引数があったときになんだかわからないためrenameしてcentralPtrに統一

--- Sources/SamplePlugin/CB4UCentralManager.swift
+++ Sources/SamplePlugin/CB4UCentralManager.swift
@@ -5,19 +5,47 @@ import CoreBluetooth
 public class CB4UCentralManager: NSObject, CBCentralManagerDelegate {
     private var centralManager: CBCentralManager!
     var didUpdateStateHandler: CB4UCentralManagerDidUpdateStateHandler?
-
+    var didDiscoverPeripheralHandler: CB4UCentralManagerDidDiscoverPeripheralHandler?
+    
     public override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
+    
     func selfPointer() -> UnsafeMutableRawPointer {
         return Unmanaged.passUnretained(self).toOpaque()
     }
-
+    
+    // MARK: - Scanning or Stopping Scans of Peripherals
+    
+    // TODO: options argument is not supported yet.
+    public func scanForPeripherals(withServices serviceUUIDs: [CBUUID]?) {
+        let options: [String: Any] = [
+            CBCentralManagerScanOptionAllowDuplicatesKey: false
+        ]
+        
+        centralManager.scanForPeripherals(withServices: serviceUUIDs, options: options)
+    }
+    
+    public func stopScan() {
+        centralManager.stopScan()
+    }
+    
+    public var isScanning: Bool {
+        return centralManager.isScanning
+    }
+    
     // MARK: - CBCentralManagerDelegate
-
+    
     public func centralManagerDidUpdateState(_ central: CBCentralManager) {
         didUpdateStateHandler?(selfPointer(), Int32(central.state.rawValue))
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        peripheral.identifier.uuidString.withCString { (uuidCString) in
+            (peripheral.name ?? "").withCString { (nameCString) in
+                didDiscoverPeripheralHandler?(selfPointer(), uuidCString, nameCString)
+            }
+        }
     }
 }
--- Sources/SamplePlugin/CoreBluetoothForUnity.swift
+++ Sources/SamplePlugin/CoreBluetoothForUnity.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreBluetooth
 
 @_cdecl("cb4u_central_manager_new")
 public func cb4u_central_manager_new() -> UnsafeMutableRawPointer {
@@ -7,18 +8,48 @@ public func cb4u_central_manager_new() -> UnsafeMutableRawPointer {
 }
 
 @_cdecl("cb4u_central_manager_release")
-public func cb4u_central_manager_release(_ instancePtr: UnsafeRawPointer) {
-    Unmanaged<CB4UCentralManager>.fromOpaque(instancePtr).release()
+public func cb4u_central_manager_release(_ centralPtr: UnsafeRawPointer) {
+    Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).release()
 }
 
 public typealias CB4UCentralManagerDidUpdateStateHandler = @convention(c) (UnsafeRawPointer, Int32) -> Void
+public typealias CB4UCentralManagerDidDiscoverPeripheralHandler = @convention(c) (UnsafeRawPointer, UnsafePointer<CChar>, UnsafePointer<CChar>) -> Void
 
 @_cdecl("cb4u_central_manager_register_handlers")
 public func cb4u_central_manager_register_handlers(
-    _ instancePtr: UnsafeRawPointer,
-    _ didUpdateStateHandler: @escaping CB4UCentralManagerDidUpdateStateHandler
+    _ centralPtr: UnsafeRawPointer,
+    _ didUpdateStateHandler: @escaping CB4UCentralManagerDidUpdateStateHandler,
+    _ didDiscoverPeripheralHandler: @escaping CB4UCentralManagerDidDiscoverPeripheralHandler
 ) {
-    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(instancePtr).takeUnretainedValue()
+    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).takeUnretainedValue()
     
     instance.didUpdateStateHandler = didUpdateStateHandler
+    instance.didDiscoverPeripheralHandler = didDiscoverPeripheralHandler
+}
+
+@_cdecl("cb4u_central_manager_scan_for_peripherals")
+public func cb4u_central_manager_scan_for_peripherals(_ centralPtr: UnsafeRawPointer, _ serviceUUIDs: UnsafePointer<UnsafePointer<CChar>?>, _ serviceUUIDsCount: Int32) {
+    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).takeUnretainedValue()
+    
+    let serviceUUIDsArray = (0..<Int(serviceUUIDsCount)).map { index -> CBUUID in
+        let uuidString = String(cString: serviceUUIDs[index]!)
+        return CBUUID(string: uuidString)
+    }
+    
+    instance.scanForPeripherals(withServices: serviceUUIDsArray)
+}
+
+
+@_cdecl("cb4u_central_manager_stop_scan")
+public func cb4u_central_manager_stop_scan(_ centralPtr: UnsafeRawPointer) {
+    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).takeUnretainedValue()
+    
+    instance.stopScan()
+}
+
+@_cdecl("cb4u_central_manager_is_scanning")
+public func cb4u_central_manager_is_scanning(_ centralPtr: UnsafeRawPointer) -> Bool {
+    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).takeUnretainedValue()
+    
+    return instance.isScanning
 }
